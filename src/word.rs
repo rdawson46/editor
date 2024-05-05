3e4e6c8db786b5aa0a4e944d305ba64436420fbd
rdawson46 <dawson4623@gmail.com>
2024-05-05 08:04:11 -0400

    removed todo

--- src/word.rs
+++ src/word.rs
@@ -19,7 +19,6 @@ impl CharKind {
     }
 }
 
-// TODO: fix this functions
 pub fn find_word_start_forward(line: &String, start_col: usize) -> Option<usize> {
     let mut it = line.chars().enumerate().skip(start_col);
     let mut prev = CharKind::new(it.next()?.1);
