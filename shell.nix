84973f175b58bbd42745acc47ed5361e04a4bbd4
rdawson46 <dawson4623@gmail.com>
2024-08-05 17:42:13 -0400

    modified shell.nix

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
     buildInputs = with pkgs; [
         cargo
         rustc
+        rust-analyzer
     ];
     RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
